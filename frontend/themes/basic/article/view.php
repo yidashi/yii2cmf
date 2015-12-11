52f4edcc9c7d43f5b7edb2b68f799ce5f8c94e07
刘俊涛 <liujuntaor@qq.com>
2015-12-11 16:04:59 +0800

    add comment

--- frontend/themes/basic/article/view.php
+++ frontend/themes/basic/article/view.php
@@ -27,11 +27,21 @@ $this->params['breadcrumbs'][] = $title;
     </div>
     <div class="view-content"><?= \yii\helpers\Markdown::process($content) ?></div>
     <h4>评论</h4>
-    <div class="col-4">
-        <?=\common\widgets\markdown\Markdown::widget([
-            'name'=>'content',
-            'value'=>'',
-            'options'=>['style'=>'height:200px;']
-        ])?>
-    </div>
+    <!-- 多说评论框 start -->
+    <div class="ds-thread" data-thread-key="<?=$id?>" data-title="<?=$title?>" data-url="<?=Yii::$app->request->url?>"></div>
+    <!-- 多说评论框 end -->
+    <!-- 多说公共JS代码 start (一个网页只需插入一次) -->
+    <script type="text/javascript">
+        var duoshuoQuery = {short_name:"51siyuan"};
+        (function() {
+            var ds = document.createElement('script');
+            ds.type = 'text/javascript';ds.async = true;
+            ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+            ds.charset = 'UTF-8';
+            (document.getElementsByTagName('head')[0]
+            || document.getElementsByTagName('body')[0]).appendChild(ds);
+        })();
+    </script>
+    <!-- 多说公共JS代码 end -->
 </div>
+
