8a78138e9fe176f3532f0b2309d2d2408a495360
刘俊涛 <liujuntao@corp.1jiajie.com>
2015-12-12 15:10:45 +0800

    添加热门

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -18,7 +18,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/font-awesome.min.css',
+        'css/font-awesome/css/font-awesome.min.css',
         'css/site.css'
     ];
     public $js = [
