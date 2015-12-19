510e809974df6cc9cde597a7fced4b15a0eef055
刘俊涛 <liujuntao@corp.1jiajie.com>
2015-12-19 21:26:32 +0800

    backend

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -74,9 +74,11 @@ AppAsset::register($this);
     NavBar::end();
     ?>
     <div class="container">
+        <?php if(strncmp(Yii::$app->request->headers->get('User-Agent'), 'MicroMessenger') !== 0): ?>
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
+        <?php endif; ?>
         <?= Alert::widget() ?>
         <?= $content ?>
     </div>
