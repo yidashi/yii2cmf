09cccb926659a941e0131fbf14cd18cb88afe235
刘俊涛 <liujuntaor@qq.com>
2015-12-23 15:49:24 +0800

    weixin

--- frontend/themes/basic/article/view.php
+++ frontend/themes/basic/article/view.php
@@ -115,7 +115,7 @@ $this->params['breadcrumbs'][] = $model->title;
     });
 js
 );
-if(stripos(Yii::$app->request->headers->get('User-Agent'), 'MicroMessenger') === false) {
+if(stripos(Yii::$app->request->headers->get('User-Agent'), 'MicroMessenger') !== false) {
     $this->registerJsFile('http://res.wx.qq.com/open/js/jweixin-1.0.0.js');
     $this->registerJs(<<<js
 wx.onMenuShareAppMessage({
