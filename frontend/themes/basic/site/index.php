d4115c1ad9509970f1da618629e11cdbec371ca2
刘俊涛 <liujuntaor@qq.com>
2015-12-23 16:26:03 +0800

    image

--- frontend/themes/basic/site/index.php
+++ frontend/themes/basic/site/index.php
@@ -12,7 +12,7 @@ $this->registerMetaTag(['property' => 'qc:admins', 'content' => '376655717261261
             <?php foreach($news as $item):?>
             <div class="col-sm-6 col-md-3 article-item">
                 <a href="<?= \yii\helpers\Url::toRoute(['article/view','id'=>$item['id']])?>">
-                <img onerror="this.src='http://www.tiejiong.com/uploads/allimg/c151206/14493S94614C0-4Ic7.png'" src="<?= $item['cover'] ? (Yii::getAlias('@static') . $item['cover']) : 'http://www.tiejiong.com/uploads/allimg/c151206/14493S94614C0-4Ic7.png'?>" alt="<?= $item['title']?>" width="200" height="145">
+                    <img onerror="this.src='http://www.tiejiong.com/uploads/allimg/c151206/14493S94614C0-4Ic7.png'" src="<?= $item['cover'] ? ( strpos($item['cover'], 'http://') === false ? (Yii::getAlias('@static') . $item['cover']) : $item['cover']) : 'http://www.tiejiong.com/uploads/allimg/c151206/14493S94614C0-4Ic7.png'?>" alt="<?= $item['title']?>" width="200" height="145">
                 <h3 class="article-title"><?= $item['title']?></h3>
                 </a>
             </div>
