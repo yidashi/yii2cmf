f3b16697f5a518188c0f6e0bf63cc0f842529d69
刘俊涛 <liujuntaor@qq.com>
2015-12-24 11:41:06 +0800

    mianzeshengm

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -94,15 +94,15 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; <?= Yii::$app->name . ' ' . date('Y') ?></p>
-
-        <p class="pull-right">京ICP备15065774号</p>
+        <div class="row">
+            <div class="col-lg-6"><a href="<?= \yii\helpers\Url::to(['/site/about'])?>">免责声明</a></div>
+            <div class="col-lg-6"><a href="<?= \yii\helpers\Url::to(['/site/about'])?>">关于我们</a></div>
+        </div>
     </div>
+    <hr/>
     <div class="container">
-        本网站所收集的部分公开资料来源于互联网，转载的目的在于传递更多信息及用于网络分享，并不代表本站赞同其观点和对其真实性负责，也不构成任何其他建议。本站部分作品是由网友自主投稿和发布、编辑整理上传，对此类作品本站仅提供交流平台，不为其版权负责。如果您发现网站上有侵犯您的知识产权的作品，请与我们取得联系，我们会及时修改或删除。
-        本网站所提供的信息，只供参考之用。本网站不保证信息的准确性、有效性、及时性和完整性。本网站及其雇员一概毋须以任何方式就任何信息传递或传送的失误、不准确或错误，对用户或任何其他人士负任何直接或间接责任。在法律允许的范围内，本网站在此声明，不承担用户或任何人士就使用或未能使用本网站所提供的信息或任何链接所引致的任何直接、间接、附带、从属、特殊、惩罚性或惩戒性的损害赔偿。
-        电子邮件：332672087#qq.com（发邮件时，请将“#”替换为“@”）
-        联系电话：18045665692
+        <p class="pull-left">&copy; <?= Yii::$app->name . ' ' . date('Y') ?></p>
+        <p class="pull-right">京ICP备15065774号</p>
     </div>
 </footer>
 <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
