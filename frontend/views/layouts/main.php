9f0df0778c8535553d13fa8c2a81c7cef25f936d
刘俊涛 <liujuntaor@qq.com>
2015-12-28 16:36:17 +0800

    站内搜索

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -46,11 +46,11 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav'],
         'items' => $menuItems,
     ]);
-    /*echo '<form class="navbar-form navbar-left" role="search">
+    echo '<form class="navbar-form navbar-left" role="search">
         <div class="form-group">
-        <input type="text" class="form-control" placeholder="搜索">
+        <input type="text" class="form-control" placeholder="搜索" id="bdcsMain">
         </div><button type="submit" class="btn btn-default"><span class="fa fa-search"></span></button>
-      </form>';*/
+      </form>';
     $rightMenuItems = [];
     $rightMenuItems[] = ['label' => '反馈', 'url' => ['/suggest/create']];
     $rightMenuItems[] = ['label' => '投稿', 'url' => ['/my/create-article']];
@@ -124,6 +124,7 @@ AppAsset::register($this);
         s.parentNode.insertBefore(bp, s);
     })();
 </script>
+<script type="text/javascript">(function(){document.write(unescape('%3Cdiv id="bdcs"%3E%3C/div%3E'));var bdcs = document.createElement('script');bdcs.type = 'text/javascript';bdcs.async = true;bdcs.src = 'http://znsv.baidu.com/customer_search/api/js?sid=4121821758462423263' + '&plate_url=' + encodeURIComponent(window.location.href) + '&t=' + Math.ceil(new Date()/3600000);var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(bdcs, s);})();</script>
 <?php $this->endBody() ?>
 </body>
 </html>
