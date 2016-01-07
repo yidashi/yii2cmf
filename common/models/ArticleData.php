ddef789d36415750af3c374d0329e591d96888bc
刘俊涛 <liujuntaor@qq.com>
2016-01-07 11:44:04 +0800

    分表玩一下

---
+++ common/models/ArticleData.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%article_data}}".
+ *
+ * @property integer $id
+ * @property string $content
+ */
+class ArticleData extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%article_data}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'content'], 'required'],
+            [['id'], 'integer'],
+            [['content'], 'string']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'content' => '内容',
+        ];
+    }
+}
