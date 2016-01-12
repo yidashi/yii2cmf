e90cb86fef16fd54d6f402b9a17352cf2a352882
刘俊涛 <liujuntaor@qq.com>
2016-01-12 11:19:07 +0800

    test

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'runtimePath' => dirname(dirname(__DIR__)) . '/runtime',
     'timezone'=>'PRC',
     'language' => 'zh-CN',
     'components' => [
