006ce0e219374217e847a1fecc6487cd17271227
yidashi <liujuntaor@qq.com>
2016-03-15 17:45:53 +0800

    hehe

--- backend/views/nav/index.php
+++ backend/views/nav/index.php
@@ -6,12 +6,10 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = '导航';
+$this->title = '导航（前台)）';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="nav-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
         <?= Html::a('创建导航', ['create'], ['class' => 'btn btn-success']) ?>
