cff0fe9ed6f6f727c06840a67671c288eaf10b9b
yidashi <liujuntaor@qq.com>
2016-03-22 18:40:38 +0800

    bug

--- backend/views/category/index.php
+++ backend/views/category/index.php
@@ -11,8 +11,6 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="category-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
         <?= Html::a('添加分类', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
--- backend/views/page/index.php
+++ backend/views/page/index.php
@@ -11,8 +11,6 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="page-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
         <?= Html::a('创建单页', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
