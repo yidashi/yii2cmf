52c782c900fe9ee403a3e431a1d4c18face39a76
yidashi <liujuntaor@qq.com>
2016-05-05 16:09:47 +0800

    fix an bug

--- backend/views/article/_form.php
+++ backend/views/article/_form.php
@@ -26,7 +26,7 @@ use yii\widgets\ActiveForm;
 
             <?= $form->field($model, 'cover')->widget('yidashi\webuploader\Webuploader') ?>
 
-            <?= $form->field($model, 'status')->radioList([1 => '激活']) ?>
+            <?= $form->field($model, 'status')->radioList(['待审', '激活']) ?>
 
         </div>
         <div class="tab-pane" id="tab_2">
