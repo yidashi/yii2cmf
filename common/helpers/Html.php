65b0803cb03fa0daf3eb87ed513253664818e8c7
yidashi <liujuntaor@qq.com>
2016-06-01 15:01:17 +0800

    签到

--- common/helpers/Html.php
+++ common/helpers/Html.php
@@ -10,11 +10,24 @@ namespace common\helpers;
 
 
 use rmrevin\yii\fontawesome\FA;
+use yii\helpers\Url;
 
 class Html extends \yii\helpers\Html
 {
     public static function icon($name, $options = [])
     {
         return FA::i($name, $options);
+    }
+
+    public static function img($src, $options = [])
+    {
+        $options['src'] = Url::to($src);
+        if (strpos($options['src'], 'http') === false) {
+            $options['src'] = \Yii::getAlias('@static') . '/' . $options['src'];
+        }
+        if (!isset($options['alt'])) {
+            $options['alt'] = '';
+        }
+        return static::tag('img', '', $options);
     }
 }
