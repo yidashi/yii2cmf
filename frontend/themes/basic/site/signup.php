cb8075841dc1209d6be1b5729dc86c41a6bacbdc
yidashi <liujuntaor@qq.com>
2016-06-01 14:57:08 +0800

    签到

--- frontend/themes/basic/site/signup.php
+++ frontend/themes/basic/site/signup.php
@@ -7,7 +7,7 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = Yii::t('app', 'Signup');
+$this->title = Yii::t('common', 'Signup');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-signup">
