f1119dc1106cbddff837971d7bb4bc20c8c22997
yidashi <liujuntaor@qq.com>
2016-06-07 00:48:52 +0800

    留言板修改

--- frontend/themes/basic/suggest/index.php
+++ frontend/themes/basic/suggest/index.php
@@ -10,6 +10,9 @@
 <div class="col-4">
     <?= \yii\widgets\ListView::widget([
         'dataProvider' => $dataProvider,
+        'viewParams' => [
+            'commentModel' => $model
+        ],
         'itemView' => '_item',
         'itemOptions' => [
             'class' => 'media',
