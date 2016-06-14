052276e4b06ea06df6f57e431e6df616e455ef50
yidashi <liujuntaor@qq.com>
2016-06-14 14:15:45 +0800

    test

--- api/common/controllers/Controller.php
+++ api/common/controllers/Controller.php
@@ -22,7 +22,7 @@ class Controller extends \yii\rest\Controller
                 'Origin' => ['*'],
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                 'Access-Control-Request-Headers' => ['*'],
-                'Access-Control-Allow-Credentials' => null,
+                'Access-Control-Allow-Credentials' => true,
                 'Access-Control-Max-Age' => 86400,
                 'Access-Control-Expose-Headers' => ['Set-Cookie', 'X-Pagination-Current-Page']
             ]
