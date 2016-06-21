6d3a865ded4c26a180e79e9f37c8ab402d3ca0e1
yidashi <liujuntaor@qq.com>
2016-06-21 15:39:09 +0800

    api

--- api/common/controllers/Controller.php
+++ api/common/controllers/Controller.php
@@ -19,12 +19,12 @@ class Controller extends \yii\rest\Controller
         $behaviors['cors'] = [
             'class' => Cors::className(),
             'cors' => [
-                'Origin' => ['http://127.0.0.1'],
+                'Origin' => ['*'],
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                 'Access-Control-Request-Headers' => ['*'],
                 'Access-Control-Allow-Credentials' => true,
                 'Access-Control-Max-Age' => 86400,
-                'Access-Control-Expose-Headers' => ['Set-Cookie', 'X-Pagination-Current-Page']
+                'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page']
             ]
         ];
         return $behaviors;
