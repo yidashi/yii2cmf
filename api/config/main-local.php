fa6f518f916c2b91a6f389b043b3bb681d38ecda
yidashi <liujuntaor@qq.com>
2016-06-21 15:34:08 +0800

    api

---
+++ api/config/main-local.php
@@ -0,0 +1,10 @@
+<?php
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'jtLjetKXn46CHLywv0aQhb0FMV5IzMNF',
+        ],
+    ],
+];
+return $config;
