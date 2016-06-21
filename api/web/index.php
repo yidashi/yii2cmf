533dec1dd768cc537024831c5f51f45326e36a43
yidashi <liujuntaor@qq.com>
2016-06-21 15:35:44 +0800

    api

--- api/web/index.php
+++ api/web/index.php
@@ -1,8 +1,13 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
 require(__DIR__ . '/../../vendor/autoload.php');
+
+// Environment
+(new \Dotenv\Dotenv(dirname(dirname(__DIR__))))->load();
+
+defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG'));
+defined('YII_ENV') or define('YII_ENV', env('YII_ENV', 'prod'));
+
+
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/bootstrap.php');
 require(__DIR__ . '/../config/bootstrap.php');
