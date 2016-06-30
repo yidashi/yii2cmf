c38098f6c6069a00c4ac7dd0fc68a68ec37d0d43
yidashi <liujuntaor@qq.com>
2016-06-30 15:21:12 +0800

    简单搜索

--- frontend/controllers/SearchController.php
+++ frontend/controllers/SearchController.php
@@ -11,17 +11,22 @@ namespace frontend\controllers;
 
 use common\models\Article;
 use yii\data\ActiveDataProvider;
+use yii\web\BadRequestHttpException;
 use yii\web\Controller;
 
 class SearchController extends Controller
 {
     /**
      * 简单先来一个搜索,搜索太大太深
-     * @param $word string
+     * @param $q string 关键词
      * @return string
+     * @throws BadRequestHttpException
      */
     public function actionIndex($q)
     {
+        if (empty($q)) {
+            throw new BadRequestHttpException('搜索关键词不能为空');
+        }
         $dataProvider = new ActiveDataProvider([
             'query' => Article::find()->published()->andWhere(['like', 'title', $q])
         ]);
--- frontend/themes/basic/site/error.php
+++ frontend/themes/basic/site/error.php
@@ -11,7 +11,7 @@ $this->title = $name;
 ?>
 <div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($message) ?></h1>
     <div class="error-search">
         <form action="<?= url(['/search']) ?>" method="get">
             <div class="input-group">
