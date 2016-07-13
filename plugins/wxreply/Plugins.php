468c9263ab3d7fd8b9ac9833ce3df7c25a56e81e
yidashi <liujuntaor@qq.com>
2016-07-13 16:02:36 +0800

    try wechat

--- plugins/wxreply/Plugins.php
+++ plugins/wxreply/Plugins.php
@@ -11,6 +11,14 @@ namespace plugins\wxreply;
 
 class Plugins extends \plugins\Plugins
 {
+    public $info = [
+        'author' => '易大师',
+        'version' => 'v1.0',
+        'name' => 'wxreply',
+        'title' => '微信自动回复',
+        'desc' => '微信自动回复'
+    ];
+
     public function wechat($app)
     {
         $app->events->addListener('yii\web\Controller','afterAction', 'plugins\wxreply\ReplyListener');
