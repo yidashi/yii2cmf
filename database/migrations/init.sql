e79e073ec789ddc3b636026d382a68876d889b9d
yidashi <liujuntaor@qq.com>
2016-07-14 11:21:46 +0800

    bug

--- database/migrations/init.sql
+++ database/migrations/init.sql
@@ -60,7 +60,7 @@ INSERT INTO {{%config}} VALUES
 (8,'FOOTER','','','底部','textarea',0,1461937892),
 (9,'THEME_NAME','basic','basic=>basic','主题名','select',0,1461937892),
 (10,'BACKEND_SKIN','skin-blue','skin-black=>skin-black\r\nskin-blue=>skin-blue\r\nskin-green=>skin-green\r\nskin-purple=>skin-purple\r\nskin-red=>skin-red\r\nskin-yellow=>skin-yellow','后台皮肤','select',1461931367,1461937892),
-(11,'FRIENDLY_LINK','Yii2CMF=>http://www.51siyuan.cn','','友情链接','array',0,1461937892);
+(11,'FRIENDLY_LINK','Yii2CMF=>http://www.51siyuan.cn','','友情链接','array',0,1461937892),
 (12,'WX_TOKEN','','','微信token','text',0,1461937892);
 /*!40000 ALTER TABLE {{%config}} ENABLE KEYS */;
 UNLOCK TABLES;
