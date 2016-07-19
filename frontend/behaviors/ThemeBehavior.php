556363e348a04822d44b6d83fa4a4f28bddb2fe2
yidashi <liujuntaor@qq.com>
2016-07-19 14:06:35 +0800

    bug

--- frontend/behaviors/ThemeBehavior.php
+++ frontend/behaviors/ThemeBehavior.php
@@ -9,20 +9,48 @@ namespace frontend\behaviors;
 use Detection\MobileDetect;
 use yii\base\ActionFilter;
 use Yii;
+use yii\web\Cookie;
 
 class ThemeBehavior extends ActionFilter
 {
     public $themeParam = 'theme';
+
+    public $themeCookieName = 'localThemeName';
     public function beforeAction($action)
     {
+        $themeName = $this->resolveTheme();
+        $this->setTheme($themeName);
+        return true;
+    }
+
+    public function resolveTheme()
+    {
+        //先看参数
         if (!$themeName = Yii::$app->request->get($this->themeParam)) {
-            $isMobile = (new MobileDetect())->isMobile();
-            if ($isMobile) {
-                $themeName = \Yii::$app->config->get('MOBILE_THEME_NAME', 'mobile');
+            //再检查cookie
+            if (Yii::$app->request->cookies->has($this->themeCookieName)) {
+                $themeName = Yii::$app->request->cookies->get($this->themeCookieName);
             } else {
-                $themeName = \Yii::$app->config->get('THEME_NAME', 'basic');
+                //最后读系统默认
+                $isMobile = (new MobileDetect())->isMobile();
+                if ($isMobile) {
+                    $themeName = \Yii::$app->config->get('MOBILE_THEME_NAME', 'mobile');
+                } else {
+                    $themeName = \Yii::$app->config->get('THEME_NAME', 'basic');
+                }
             }
         }
+        //设置cookie
+        Yii::$app->response->cookies->add(new Cookie([
+            'name' => $this->themeCookieName,
+            'value' => $themeName,
+            'expire' => 0 // 默认0,浏览器关闭cookie失效
+
+        ]));
+        return $themeName;
+    }
+    public function setTheme($themeName)
+    {
         $theme = [
             'class' => 'yii\base\Theme',
             'basePath' => '@frontend/themes/' . $themeName,
@@ -34,8 +62,6 @@ class ThemeBehavior extends ActionFilter
                 ],
             ],
         ];
-//        $themeInfo = Yii::createObject('frontend\themes\\' . $themeName . '\Theme');
         \Yii::$app->view->theme = \Yii::createObject($theme);
-        return true;
     }
 }
