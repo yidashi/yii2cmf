cf9ad2ad6041aa2ca71187141e2231150bd07f51
yidashi <liujuntaor@qq.com>
2016-07-28 14:11:36 +0800

    修复后台操作记录日志bug

--- common/modules/user/views/admin/_search.php
+++ common/modules/user/views/admin/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model backend\models\UserSearch */
+/* @var $model common\modules\user\models\UserSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
