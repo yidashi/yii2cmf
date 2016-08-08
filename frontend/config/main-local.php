f6cd03ce7e92d11513bf3317949f780c3e4c6c33
yidashi <liujuntaor@qq.com>
2016-08-08 13:59:20 +0800

    bug

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: yidashi
- * Date: 16/8/7
- * Time: 下午6:05
- */
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => env('FRONTEND_COOKIE_VALIDATION_KEY')
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => env('FRONTEND_PRETTY_URL', 'false'),
+            'showScriptName' => false,
+            'rules' => [
+                [
+                    'pattern' => '<id:\d+>',
+                    'route' => 'article/view',
+                    'suffix' => '.html'
+                ],
+                '/' => 'site/index',
+                '<controller:\w+>' => '<controller>/index',
+                'tag/search' => '/tag/search',
+                'tag/<name:\S+>' => '/article/tag'
+            ],
+        ]
+    ],
+];
+return $config;
