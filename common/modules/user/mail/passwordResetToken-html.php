ddf425eaee663c050afd29513e5390546e011536
yidashi <liujuntaor@qq.com>
2016-08-09 18:35:04 +0800

    前台找回密码验证邮箱规则错误

--- common/modules/user/mail/passwordResetToken-html.php
+++ common/modules/user/mail/passwordResetToken-html.php
@@ -6,10 +6,20 @@ use yii\helpers\Html;
 
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['/user/security/reset-password', 'token' => $user->password_reset_token]);
 ?>
-<div class="password-reset">
-    <p>Hello <?= Html::encode($user->username) ?>,</p>
 
-    <p>Follow the link below to reset your password:</p>
-
-    <p><?= Html::a(Html::encode($resetLink), $resetLink) ?></p>
-</div>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+    <?= Yii::t('user', 'Hello') ?>,
+</p>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+    <?= Yii::t('user', 'We have received a request to reset the password for your account on {0}', Yii::$app->config->get('SITE_NAME')) ?>.
+    <?= Yii::t('user', 'Please click the link below to complete your password reset') ?>.
+</p>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+    <?= Html::a(Html::encode($resetLink), $resetLink) ?>
+</p>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+    <?= Yii::t('user', 'If you cannot click the link, please try pasting the text into your browser') ?>.
+</p>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+    <?= Yii::t('user', 'If you did not make this request you can ignore this email') ?>.
+</p>
