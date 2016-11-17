dce88cb822874161c7f4de2fdb730d80240f2c2e
yidashi <liujuntaor@qq.com>
2016-11-17 23:01:15 +0800

    bug

--- database/migrations/m160728_025305_create_config_table.php
+++ database/migrations/m160728_025305_create_config_table.php
@@ -21,7 +21,7 @@ class m160728_025305_create_config_table extends Migration
             'id' => $this->primaryKey(),
             'name' => $this->string(50)->notNull()->comment('配置名'),
             'value' => $this->text()->comment('配置值'),
-            'extra' => $this->text()->defaultValue('')->notNull(),
+            'extra' => $this->text()->notNull(),
             'description' => $this->string(255)->comment('配置描述'),
             'type' => $this->string(30)->defaultValue('text')->comment('配置类型'),
             'created_at' => $this->integer(10)->notNull(),
