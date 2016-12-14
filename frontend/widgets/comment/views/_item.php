7f54f0cecf61bae4bda3c64909b77d978cc4ec49
刘俊涛 <liujuntaor@qq.com>
2016-12-14 19:47:43 +0800

    hehe

--- frontend/widgets/comment/views/_item.php
+++ frontend/widgets/comment/views/_item.php
@@ -8,6 +8,7 @@
 use yii\helpers\Html;
 use yii\helpers\Markdown;
 use yii\helpers\Url;
+use yii\helpers\HtmlPurifier;
 
 ?>
 <div class="media-left">
@@ -21,7 +22,7 @@ use yii\helpers\Url;
         发表于 <?= Yii::$app->formatter->asDatetime($model->created_at, 'php:Y-m-d H:i') ?>
         <?php if($model->is_top): ?><span class="label label-primary">置顶</span><?php endif; ?>
     </div>
-    <div class="media-content" id="suggest-<?= $model->id ?>"><?= Markdown::process($model->content, 'gfm')?></div>
+    <div class="media-content" id="suggest-<?= $model->id ?>"><?= HtmlPurifier::process(Markdown::process($model->content, 'gfm')) ?></div>
     <?php foreach ($model->sons as $son):?>
         <div class="media">
             <div class="media-left">
@@ -34,7 +35,7 @@ use yii\helpers\Url;
                     <a href="<?= Url::to(['/user/default/index', 'id' => $son->user_id])?>" rel="author" data-original-title="<?=$son->user->username?>" title=""><?=$son->user->username?></a> 回复于 <?=date('Y-m-d H:i', $son->created_at)?>
                     <span class="pull-right"><a class="reply-btn j_replayAt" href="javascript:;">回复</a></span>
                 </div>
-                <div class="media-content" id="suggest-<?= $son->id ?>"><?= Markdown::process(\common\models\Comment::process($son->content))?></div>
+                <div class="media-content" id="suggest-<?= $son->id ?>"><?= HtmlPurifier::process(Markdown::process(\common\models\Comment::process($son->content))) ?></div>
             </div>
         </div>
     <?php endforeach;?>
