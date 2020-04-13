b5f2bc2a42d769cca574c9306415a1c9a671e1d5
liujuntao <liujuntao@iautos.cn>
2020-04-13 18:44:40 +0800

    删了rbac的外键

--- install/views/site/selectmodule.php
+++ install/views/site/selectmodule.php
@@ -1,6 +1,10 @@
 <h2>请选择要安装的模块：</h2>
 <form method="post" class="install-form">
     <?php foreach ($modules as $module): ?>
-        <label><input type="checkbox" name="modules[]" value="<?= $module->package ?>" <?php if ($module->isCore): ?>disabled<?php endif; ?> checked/> <?= $module->name ?></label>
+        <?php if ($module->isCore): ?>
+            <label><input type="checkbox" name="modules[]" value="<?= $module->package ?>" disabled checked/> <?= $module->name ?></label>
+        <?php else: ?>
+            <label><input type="checkbox" name="modules[]" value="<?= $module->package ?>"/> <?= $module->name ?></label>
+        <?php endif; ?>
     <?php endforeach; ?>
 </form>
